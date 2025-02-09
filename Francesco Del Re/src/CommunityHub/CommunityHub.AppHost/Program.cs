cd713838c31740dfe802af9850089e7b1f261533
Francesco Del Re <francesco.delre.87@gmail.com>
2025-02-09 20:06:00 +0100

    feat: add PostgreSQL connection

--- Francesco Del Re/src/CommunityHub/CommunityHub.AppHost/Program.cs
+++ Francesco Del Re/src/CommunityHub/CommunityHub.AppHost/Program.cs
@@ -1,10 +1,17 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.CommunityHub_Api>("apiservice");
+var postgres = builder.AddPostgres("postgres")
+                      //.WithImage("docker.io/library/postgres:latest")
+                      .WithPgWeb();
+var postgresdb = postgres.AddDatabase("postgresdb");
+
+var apiService = builder.AddProject<Projects.CommunityHub_Api>("apiservice")
+    .WithReference(postgresdb);
 
 builder.AddProject<Projects.CommunityHub_Frontend>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService)
+    .WithReference(postgresdb)
     .WaitFor(apiService);
 
 builder.Build().Run();
