43abcab7eb7ea0c906674b73edf7650085bf4fb3
Fabio Mannis <fabio.mannis@outlook.com>
2025-02-21 18:28:59 +0100

    clean sln e refactor

--- Fabio Mannis/src/AnagraficaPazienti.Domain/Entities/Paziente.cs
+++ Fabio Mannis/src/AnagraficaPazienti.Domain/Entities/Paziente.cs
@@ -3,9 +3,9 @@
     public class Paziente
     {
         public int Id { get; set; }
-        public string Nome { get; set; } = string.Empty;
-        public string Cognome { get; set; } = string.Empty;
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
         public DateTime DataNascita { get; set; }
-        public string CodiceFiscale { get; set; } = string.Empty;
+        public string CodiceFiscale { get; set; }
     }
 }
